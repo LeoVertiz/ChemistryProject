ec208a35c21a243cfd6d632695d6e86050a3cf1e
Leonardo Vertiz <leovertiz@outlook.com>
2020-04-02 03:17:58 -0600

    First version of index

---
+++ Scripts/createCards.js
@@ -0,0 +1,38 @@
+var getCards = () => {
+    for(let i = 0; i < components.length; i++){
+        let component = components[i];
+        let containerId = i < 4 ? 'components1' : 'components2';
+        let container = document.getElementById(containerId);
+        let theme = (i < 4 && !(i % 2)) || (i >= 4 && i % 2) ? 'bg-dark' : 'bg-secondary' ;
+
+        let card = document.createElement('div');
+        card.className = "card col-md-3 text-white " + theme;
+
+        let image = document.createElement('img');
+        image.src = './Images/Components/' + component.img;
+        image.className = 'card-img-top';
+
+        let content = document.createElement('div');
+        content.className = 'card-body';
+
+        let title = document.createElement('h5');
+        title.className = 'card-title';
+        title.textContent = component.title;
+        content.appendChild(title);
+
+        let text = document.createElement('p');
+        text.className = 'card-text';
+        text.textContent = component.text;
+        content.appendChild(text);
+
+        let button = document.createElement('a');
+        button.className = 'btn btn-primary';
+        button.href = '#';
+        button.textContent = 'La quimica de ' + component.title;
+        content.appendChild(button);
+
+        card.appendChild(image);
+        card.appendChild(content);
+        container.appendChild(card);
+    }
+}
---
+++ Scripts/main.js
@@ -0,0 +1,42 @@
+var components = [
+    {
+        title: "Placa Madre",
+        text: "La placa madre es un elemento fundamental en una computadora dado que es la encargada de entrelazar o comunicar todas las demás partes, placas periféricos y demás componente uno entre otro.",
+        img: "placaMadre.png"
+    },
+    {
+        title: "Procesador",
+        text: "El procesador está ubicado en el corazón de la placa madre y prácticamente es el cerebro de la computadora, científicamente es llamado CPU.",
+        img: "procesador.png"
+    },
+    {
+        title: "Memoria RAM",
+        text: "La memoria RAM es donde se va almacenando al información que se está utilizando en el momento.",
+        img: "ram.png"
+    },
+    {
+        title: "Buses de Comunicación",
+        text: "Estos son los que se encargan de ínter-conectar los componentes entre sí.",
+        img: "buses.png"
+    },
+    {
+        title: "Fuente de Poder",
+        text: "Este complemento es el que se encarga de proveer energía a todos los dispositivos conectados a la placa madre de la computadora.",
+        img: "fuente.png"
+    },
+    {
+        title: "Disco Duro",
+        text: "Son dispositivos típicos usados para almacenar información en una computadora.",
+        img: "discoDuro.png"
+    },
+    {
+        title: "Mouse",
+        text: "Sirve para ínteractuar por medio de un puntero en la pantalla del ordenador. Teclado: este es un dispositivo de entrada el cual sirve para ingresar datos mediante programas de procesamiento.",
+        img: "mouse.webp"
+    },
+    {
+        title: "Teclado",
+        text: "Este es un dispositivo de entrada el cual sirve para ingresar datos mediante programas de procesamiento.",
+        img: "teclado.png"
+    }
+];
